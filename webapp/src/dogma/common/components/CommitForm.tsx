130d3fc8c8f44c312e26b33458ef90b6bac8fbe5
yjkim <kt65346337@gmail.com>
2026-09-24 11:15:09 +0900

    Add license headers

--- webapp/src/dogma/common/components/CommitForm.tsx
+++ webapp/src/dogma/common/components/CommitForm.tsx
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026 LY Corporation
+ *
+ * LY Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 import { Button, FormControl, Heading, Input, Stack, Textarea, VStack } from '@chakra-ui/react';
 import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
